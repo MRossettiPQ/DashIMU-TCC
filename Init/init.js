87152ed82e3a739bc458c3de2247452109416811
Matheus Rossetti <matheus__rossetti@hotmail.com>
2022-05-11 12:32:59 -0300

    ajuste init

--- Init/init.js
+++ Init/init.js
@@ -1,10 +1,28 @@
-import {execSync, spawnSync} from 'child_process';
+import {execSync} from 'child_process';
 import path from 'path';
+
+async function firstInit() {
+    //TODO primeira incialização
+    execSync('nvm install 16.5.0 && nvm use 16.5.0', {stdio: 'inherit'})
+    execSync('npm install -g yarn', {stdio: 'inherit'})
+    execSync('yarn global add @quasar/cli', {stdio: 'inherit'})
+    switch (process.platform) {
+        case 'linux':
+            execSync('sudo apt -y update && sudo apt -y install curl software-properties-common gnupg2 install mariadb-server', {stdio: 'inherit'})
+            execSync('sudo mariadb-secure-installation', {stdio: 'inherit'})
+        default:
+            console.log('Sistema não reconhecido para script de instalação de dependencias');
+            break;
+    }
+}
 
 async function start() {
     try {
+        if(true){
+            await firstInit()
+        }
         //TODO realiza o build da versão de produção do Front
-        execSync('npm run build --prefix ../Front', {stdio: 'inherit'})
+        execSync('yarn --cwd ../Front build', {stdio: 'inherit'})
 
         switch (process.platform) {
             case 'linux':
@@ -13,9 +31,10 @@ async function start() {
                 break;
             case 'win32':
                 //TODO deleta a versão anterior do SPA presente na pasta de distribuição do server
-                // execSync(`del /S ${path.basename('/Server/public/*')}`)
+                execSync(`del /S ${path.basename('/Server/public/*')}`)
+
                 //TODO move os arquivos da pasta de build do quasar para a pasta public de distribuição do express
-                execSync('move ../Front/dist/spa/* ../Server/public')
+                execSync('move ../Front/dist/spa/* ../Server/public/')
                 break;
             default:
                 console.log('Sistema não reconhecido para script de inicialização');
@@ -23,7 +42,7 @@ async function start() {
         }
 
         //TODO inicializa servidor
-        execSync('npm run dev --prefix ../Server', {stdio: 'inherit'})
+        execSync('yarn --cwd ../Server dev', {stdio: 'inherit'})
 
         console.log('Tudo certo!!!');
         console.log('Servidor será iniciado!!!');
